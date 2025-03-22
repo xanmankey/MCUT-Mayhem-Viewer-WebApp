e12b75d58b7c28bfeac2dc7acb55eb24c3f0c895
xanmankey <86136010+xanmankey@users.noreply.github.com>
2025-03-22 12:51:15 -0400

    black text

--- src/components/viewer_answered.tsx
+++ src/components/viewer_answered.tsx
@@ -45,7 +45,7 @@ function ViewerAnswered() {
       className="bg-gray-200 h-screen w-screen flex flex-col items-center justify-center"
       key={location.key}
     >
-      <h1 className="text-2xl font-bold mb-4">Good answer?</h1>
+      <h1 className="text-black text-2xl font-bold mb-4">Good answer?</h1>
       <div className="w-12 h-12 border-4 border-t-4 border-t-purple-500 border-purple-300 rounded-full mt-4 animate-spin"></div>
     </div>
   );
